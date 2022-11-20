2ca52076a9e4574e65dbf9f1ccc2493ed9c72f69
tinothaking <62933993+tinothaking@users.noreply.github.com>
2022-11-20 22:36:05 +0200

    Selected Fix

---
+++ selected.js
@@ -0,0 +1,28 @@
+const matches = document.querySelectorAll(".match")
+
+//if not already selected, select it
+matches.forEach((match) => {
+    match.onclick = () => {
+        const selectedArr = document.querySelectorAll(".selected")
+        
+        if (selectedArr.length !== 0) {
+            selectedArr.forEach((selectedMatch) => {
+                selectedMatch.classList.remove("selected")
+            })
+            
+            match.classList.add("selected")
+        }
+    }  
+})
+
+const selectWinnerBtn = document.querySelector(".button.next")
+
+selectWinnerBtn.onclick = () => {
+    const selectedHomeTeam = document.querySelector(".selected>#home").innerHTML
+    const selectedAwayTeam = document.querySelector(".selected>#away").innerHTML
+
+    localStorage.setItem("selectedHomeTeam", selectedHomeTeam)
+    localStorage.setItem("selectedAwayTeam", selectedAwayTeam)
+
+    sessionStorage.setItem("Test1", "ONE")
+}
---
+++ winner.js
@@ -0,0 +1,5 @@
+document.querySelector("#home").
+    innerHTML = localStorage.getItem("selectedHomeTeam")
+
+document.querySelector("#away").
+    innerHTML = localStorage.getItem("selectedAwayTeam")
