9c8dc379a405f93449713e80f111624a2adb39e8
Owen Muchini <owenmuchini1@gmail.com>
2022-11-24 00:46:43 +0100

    adding missing files

---
+++ winner.js
@@ -0,0 +1,46 @@
+document.querySelector("#home").
+    innerHTML = localStorage.getItem("selectedHomeTeam")
+
+document.querySelector("#away").
+    innerHTML = localStorage.getItem("selectedAwayTeam")
+
+
+const teams = document.querySelectorAll('.team')
+const winInputOne = document.querySelector('#win-team-1')
+const winInputTwo = document.querySelector('#win-team-2')
+
+winInputOne.style.visibility = 'hidden'
+winInputTwo.style.visibility = 'hidden'
+
+
+function toggleSelected() {
+    this.classList.toggle('selected')
+}
+
+
+const select = teams.forEach( match => match.addEventListener('click', toggleSelected))
+//if not already selected, select it
+teams.forEach((team) => {
+    team.onclick = () => {
+        const selectedArr = document.querySelectorAll(".selected")
+        
+        if (selectedArr.length !== 0) {
+            selectedArr.forEach((selectedTeam) => {
+                selectedTeam.classList.remove("selected")
+                winInputOne.checked = false
+                winInputOne.checked = false
+            })
+            
+            team.classList.add("selected")
+
+            if(selectedArr[0].classList.contains("selected")) {
+                winInputOne.checked = true
+            }
+            if(selectedArr[1].classList.contains("selected")) {
+                winInputTwo.checked = true
+            } 
+            
+        }
+       
+    }  
+})
