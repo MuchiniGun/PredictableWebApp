c2f8d93779e755a1eac9f56f5350c1d8968e6aed
kingkonglong <119009275+JPKANDCOMPANY2022@users.noreply.github.com>
2022-12-30 07:12:29 -0500

    Initial messy working

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,24 +21,19 @@ const firebaseapp = initializeApp(firebaseConfig);
 
 
 const auth = getAuth(firebaseapp);
-signInWithPopup(auth, provider)
-  .then((result) => {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    const credential = GoogleAuthProvider.credentialFromResult(result);
-    const token = credential.accessToken;
-    // The signed-in user info.
-    const user = result.user;
+
+
+onAuthStateChanged(auth, (user) => {
+  if (user != null) {
+    console.log("user logged in")
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    const uid = user.uid;
     // ...
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GoogleAuthProvider.credentialFromError(error);
+  } else {
+    console.log("User is signed out")
     // ...
-  });
+  }
+});
 
 
 
